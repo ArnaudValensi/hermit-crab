4dad6a13c0aa942af5cef6f4a0d94ee5089f3cd7
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-01-27 13:50:15 +0100

    Add title picture

--- src/start_state.lua
+++ src/start_state.lua
@@ -1,6 +1,34 @@
+function new_sprite(options)
+    local sprite = {
+        sprite_number = options.sprite_number,
+        width_in_cell = options.width_in_cell,
+        height_in_cell = options.height_in_cell,
+        draw_at = function(self, x, y)
+            spr(self.sprite_number, x, y, self.width_in_cell, self.height_in_cell)
+        end,
+    }
+
+    return sprite
+end
+
+function new_title_picture()
+    local title_picture = {
+        draw_at = function(x, y)
+
+        end
+    }
+end
+
 function require_start_state()
+    local title_sprite = new_sprite({
+        sprite_number = 208,
+        width_in_cell = 16,
+        height_in_cell = 4,
+    })
+
     local start_state = {
         on_start = function()
+            printh('[1]', 'log');
 
         end,
 
@@ -17,7 +45,8 @@ function require_start_state()
         draw = function()
             cls()
             camera(0, 0)
-            draw_text("press 🅾️ / z to start ", 64, 120)
+            title_sprite:draw_at(0, 90)
+            -- draw_text("press 🅾️ / z to start ", 64, 120)
         end
     }
 
